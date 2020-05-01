907ce09f5f0ee1ffe9fe245d62b64f27fb329288
novatel-applications-engineering <support@novatel.com>
2020-05-01 15:34:33 -0600

    Cleaned up Docker files

--- docker/run-melodic-test.sh
+++ docker/run-melodic-test.sh
@@ -4,6 +4,7 @@ NAME=ros-melodic-test
 docker build -t $NAME - < docker/Dockerfile.melodic.test
 
 CONT_TESTDIR=/home/test
-HOST_TESTDIR=$(pwd)/docker-test
+HOST_TESTDIR=$(pwd)/docker-melodic-test
+mkdir -p $HOME_TESTDIR
 docker run -ti --mount type=bind,source=$HOST_TESTDIR,target=$CONT_TESTDIR -w $CONT_TESTDIR -u `id -u`:`id -g` --group-add sudo --rm $NAME:latest 
 
