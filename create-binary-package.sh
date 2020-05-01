e3d3505366fd2a9921ffd03308432605dedf67d3
NovAtel Applications Engineering <support@novatel.com>
2020-04-30 21:43:55 -0600

    Move liboem7decoder under novatel_oem7_driver

--- create-binary-package.sh
+++ create-binary-package.sh
@@ -3,12 +3,19 @@
 UBUNTU_TGT_DISTRO=$1
 ROS_TGT_DISTRO=$2
 
-rosdep init
+rosdep init # this usually fails with harmless error
 rosdep update
 
 rm *.deb
 rm src/*.deb
 ./create-deb.sh $UBUNTU_TGT_DISTRO $ROS_TGT_DISTRO src/novatel_oem7_msgs
+
+# reinstall the msgs to satisfy the dependencies for the driver.
+cd src
+echo build | sudo -S apt remove  -y ros-$ROS_TGT_DISTRO-novatel-oem7-msgs
+echo build | sudo -S apt install -y ./*.deb
+cd ..
+
 ./create-deb.sh $UBUNTU_TGT_DISTRO $ROS_TGT_DISTRO src/novatel_oem7_driver
 mv src/*.deb .
 
