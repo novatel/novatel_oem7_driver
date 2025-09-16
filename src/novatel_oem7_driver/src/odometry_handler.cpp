0916152b3fd6cc78427f621166088910c9199b10
Tim Clephas <tim.clephas@nobleo.nl>
2025-09-17 03:19:26 +0800

    chore: tf2_ros to hpp headers (#104)
    Signed-off-by: Tim Clephas <tim.clephas@nobleo.nl>

--- src/novatel_oem7_driver/src/odometry_handler.cpp
+++ src/novatel_oem7_driver/src/odometry_handler.cpp
@@ -31,7 +31,7 @@
 #include <driver_parameter.hpp>
 #include <novatel_oem7_driver/oem7_ros_messages.hpp>
 
-#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <oem7_ros_publisher.hpp>
